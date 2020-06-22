89f90b4f7ab7cdb705d7b22b65d8569b2ee80277
d-novitskiy <you@example.com>
2020-06-22 22:40:47 +0300

    Updates

---
+++ precache-manifest.89daf1b3d412abd1f31811fe916b6ea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e77e8dfcb81e7db0364817b45f2bbe6",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "9c9c9eacf5f00e9a5fea",
+    "url": "/movies/static/css/main.330e0122.chunk.css"
+  },
+  {
+    "revision": "0d04a95274aec33589c2",
+    "url": "/movies/static/js/2.38f4fa51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies/static/js/2.38f4fa51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9c9eacf5f00e9a5fea",
+    "url": "/movies/static/js/main.26599420.chunk.js"
+  },
+  {
+    "revision": "1241a641777761abfc2e",
+    "url": "/movies/static/js/runtime-main.daa07c4d.js"
+  },
+  {
+    "revision": "b97832d903bbe3cdd3571ef59a713e05",
+    "url": "/movies/static/media/header.b97832d9.jpg"
+  }
+]);
